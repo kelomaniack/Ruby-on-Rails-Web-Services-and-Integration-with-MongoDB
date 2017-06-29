58f2af89b6f87a2af7878b2f5f1618fb7bf7fd4f
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-06-29 17:57:15 +0200

    Fixed AddressComponent class attributes

--- places/app/models/address_component.rb
+++ places/app/models/address_component.rb
@@ -1,5 +1,9 @@
 class AddressComponent
-  attr_reader :long_name, :short_name
-  attr_reader :short_name, type: String
-  attr_reader :types, type: Array
+  attr_reader :long_name, :short_name, :types
+
+  def initialize(params = {})
+    @long_name = params[:long_name]
+    @short_name = params[:short_name]
+    @types = params[:types]
+  end
 end
