520369f1becb369e994058bd4a0c01dd3eeef398
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-07-10 14:01:47 +0200

    Finished Implement Attribute Delegation 1st step

--- triresults/app/models/racer.rb
+++ triresults/app/models/racer.rb
@@ -3,7 +3,7 @@ class Racer
   Mongo::Logger.logger.level = ::Logger::INFO
 
   embeds_one :info, class_name: 'RacerInfo', as: :parent, autobuild: true
-  # has_many :races, class_name: 'Entrant', foreign_key: "racer.racer_id", dependent: :nullify, order: :"race.date".desc
+  has_many :races, class_name: 'Entrant', foreign_key: "racer.racer_id", dependent: :nullify, order: :"race.date".desc
 
   delegate :first_name, :first_name=, to: :info
   delegate :last_name, :last_name=, to: :info
--- triresults/app/models/racer_info.rb
+++ triresults/app/models/racer_info.rb
@@ -20,4 +20,15 @@ class RacerInfo
   validates :gender, inclusion: {in: %w(M F)}
   validates :birth_year, numericality: {only_integer: true, less_than: Date.today.year, message: "Has to be in the past"}
 
+  ["city", "state"].each do |action|
+    define_method("#{action}") do
+      self.residence? ? self.residence.send("#{action}") : nil
+    end
+    define_method("#{action}=") do |name|
+      object = self.residence ||= Address.new
+      object.send("#{action}=",name)
+      self.residence = object
+    end
+  end
+
 end
